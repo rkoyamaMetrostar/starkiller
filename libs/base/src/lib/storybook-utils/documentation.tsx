b7027313182699fedff052552e02a64b9371de9f
Jonathan Bouder <johnnybouder@Jonathans-MacBook-Air.local>
2022-10-04 14:15:04 -0400

    Formatting and quality updates.

--- libs/base/src/lib/storybook-utils/documentation.tsx
+++ libs/base/src/lib/storybook-utils/documentation.tsx
@@ -2,23 +2,23 @@ import './documentation.scss';
 
 /* eslint-disable-next-line */
 export interface IDocumentationProps {
-    /**
-     * Heading for documentation
-     */
-    heading: string;
-    /**
-     * Body for documentation
-     */
-    body: string;
+  /**
+   * Heading for documentation
+   */
+  heading: string;
+  /**
+   * Body for documentation
+   */
+  body: string;
 }
 
 export function Documentation({ heading, body }: IDocumentationProps) {
-    return (
-        <>
-            <span className='documentation-heading'>{heading}</span>
-            <p>{body}</p>
-        </>
-    );
+  return (
+    <>
+      <span className="documentation-heading">{heading}</span>
+      <p>{body}</p>
+    </>
+  );
 }
 
 export default Documentation;
